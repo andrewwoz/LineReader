14f578dd6889ae4dad9a7c6a3e94d46efb2fa966
Andy Chikalo <andy.chikalo@gmail.com>
2016-11-29 00:42:36 +0200

    Refactor code. Rename class. Reduce memory footprint.
    Implemented using C i/o api.
    
    fix issues #2

---
+++ LineReader.swift
@@ -0,0 +1,38 @@
+// Copyright © 2016 andrewwoz
+
+import Foundation
+
+/// Read text file line by line
+class LineReader {
+    let path: String
+    
+    fileprivate let file: UnsafeMutablePointer<FILE>!
+    
+    init?(path: String) {
+        self.path = path
+        
+        file = fopen(path, "r")
+        
+        guard file != nil else { return nil }
+        
+    }
+    
+    var nextLine: String? {
+        var line:UnsafeMutablePointer<CChar>? = nil
+        var linecap:Int = 0
+        defer { free(line) }
+        return getline(&line, &linecap, file) > 0 ? String(cString: line!) : nil
+    }
+    
+    deinit {
+        fclose(file)
+    }
+}
+
+extension LineReader: Sequence {
+    func  makeIterator() -> AnyIterator<String> {
+        return AnyIterator<String> {
+            return self.nextLine
+        }
+    }
+}
